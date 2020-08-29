5e6eb5e870d632c555cf927101bb98268735aece
aczamaz <aczamaz@gmail.com>
2020-08-29 09:03:03 +0300

    add spinner for random planet

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './spinner.css';
+
+export default class Spinner extends Component {
+    render() {
+        return (
+            <div class="lds-roller">
+                <div>
+                </div>
+                
+                <div>
+                </div>
+                
+                <div>
+                </div>
+                
+                <div>
+                </div>
+                
+                <div>
+                </div>
+                
+                <div>
+                </div>
+                
+                <div>
+                </div>
+                
+                <div>
+                </div>
+            </div>
+        )
+    }
+}
